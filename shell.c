6e28114ba2743be8a80b414bd391fa46fe735881
matthew0367 <matthew0367@gmail.com>
2015-11-27 22:50:31 -0500

    Neatened stuff up and added in more comments

--- shell.c
+++ shell.c
@@ -22,32 +22,37 @@ void prompt() {
 }
 
 char* inputLine() {
-	/** Responsible for user interaction.
+	/** Function: Gives the shell prompt and reads in the text the user enters.
 	 * 
 	 * 	Returns:
-	 * 		Input to be executed, divided up into commands and their options.
+	 * 		The text entered into stdin with the newline char removed
 	 */
 	char sin[256];
 	char* s = (char *)malloc(sizeof(char) * 256);;
-	//char** scol = (char **)malloc(sizeof(char *) * 256);
 	prompt();
 	fgets(sin, sizeof(sin), stdin);
 	strcpy(s, sin);
-	char* news;
 	char* sp = s;
 	sp = strsep(&sp, "\n");
 	return sp;
 }
 
 char** inputCommand(char* input) {
+	/* Input: Takes in the line containing one full command (e.g. "ls -l")
+
+	   Returns: An array of strings with each element in the array containing a different section of the command 
+	   (e.g. ["ls", "-l"])
+	*/
 	char** sar = (char **)malloc(sizeof(char *) * 256);
 	char* news;
 	int c = 0;
 	while(input) {
 		news = strsep(&input, " ");
+		//make sure news isn't empty
 		while (!strcmp(news, "\0")) {
 			news = strsep(&input, " ");
 		}
+		//get rid of any extra spaces at the beginning of input
 		if (input) {
 			while (input[0] == ' ') {
 				strsep(&input, " ");
@@ -61,6 +66,12 @@ char** inputCommand(char* input) {
 }
 
 int execute(char** command) {
+	/* 	Input:  An array of strings with each element in the array containing a different section of the command
+		
+		Function: Executes the entered command
+
+		Returns: WEXITSTATUS
+	*/
 	pid_t chP = fork();
 	int status = 0;
 	if(chP == 0) {
